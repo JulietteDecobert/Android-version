7e890b092ee5f3e93c5d93a66d140fbf25d8ffba
Juliette Decobert <juliette.decobert@etu.unice.fr>
2020-10-23 15:20:26 +0200

    indentation

--- app/src/main/java/com/example/android_version/MainActivity.kt
+++ app/src/main/java/com/example/android_version/MainActivity.kt
@@ -7,18 +7,30 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val items = Array<AndVersion>(11, {AndVersion()})
+    val items = Array<AndVersion>(11, { AndVersion() })
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         seedItems()
-        recyclerView.layoutManager=LinearLayoutManager(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = AndVersionAdapter(items)
     }
-    fun seedItems(){
-        val nameArray= resources.getStringArray(R.array.name)
-        val imgArray = intArrayOf(R.drawable.cupcake, R.drawable.donut, R.drawable.eclair, R.drawable.oreo, R.drawable.nougat, R.drawable.gingerbread, R.drawable.ice_cream_sandwitch, R.drawable.jelly_bean, R.drawable.froyo,R.drawable.lollipop)
-        for(k in 0..nameArray.size-1){
+
+    fun seedItems() {
+        val nameArray = resources.getStringArray(R.array.name)
+        val imgArray = intArrayOf(
+            R.drawable.cupcake,
+            R.drawable.donut,
+            R.drawable.eclair,
+            R.drawable.oreo,
+            R.drawable.nougat,
+            R.drawable.gingerbread,
+            R.drawable.ice_cream_sandwitch,
+            R.drawable.jelly_bean,
+            R.drawable.froyo,
+            R.drawable.lollipop
+        )
+        for (k in 0..nameArray.size - 1) {
             items[k] = AndVersion(nameArray[k], imgArray[k])
         }
     }
